e7973471891ddf41527068f56f0f973f0d3c3980
Manu GB <Manu@MacBook-Air-de-manu.local>
2020-10-08 21:47:12 +0100

    nasa

--- nasa.js
+++ nasa.js
@@ -1,27 +1,39 @@
 // Fotos de astronomia
 // 1- Crea una página con tres cajas de texto en la que se le pida al usuario un año, un mes y un día. Al apretar un botón, se le hace una petición a la API de la NASA de APOD, y muestra la fecha, el título, la imágen y la descripción en la página.
 // Para usar la API de la NASA se necesita una clave de verificación.
-let dia = "";
-let mes = "";
-let anyo = "";
 
-let url = `https://api.nasa.gov/planetary/apod?api_key=gk1jxfQTterUuIrzddIX6VdTHVt4o4A8xfBRgsPf&date=${2019}-${01}-${28}`;
+// HTML
+// <body>
+// <!-- Enviar fecha -->
+// <input type="text" name="dia" placeholder="dd" id="dia" />
+// <input type="text" name="mes" placeholder="mm" id="mes" />
+// <input type="text" name="anyo" placeholder="anyo" id="anyo" />
+// <button onclick="verFecha()">Mostrar evento</button>
 
-function verFecha() {
-  dia = parseInt(document.getElementById("dia").value);
-  mes = parseInt(document.getElementById("mes").value);
-  anyo = parseInt(document.getElementById("anyo").value);
-  console.log(dia, mes, anyo);
+// <!-- Mostrar resultado -->
+// <div id="mostrar"></div>
+// <script src="./nasa.js"></script>
+// </body>
+
+let url = `https://api.nasa.gov/planetary/apod?api_key=gk1jxfQTterUuIrzddIX6VdTHVt4o4A8xfBRgsPf&date=`;
+
+function sacarFecha() {
+  let fecha = `${document.getElementById("anyo").value}-${
+    document.getElementById("mes").value
+  }-${document.getElementById("dia").value}`;
+  return fecha;
 }
 
-fetch(url)
-  .then(function recogerRespuesta(respuesta) {
-    return respuesta.json();
-  })
-  .then(function recogerDatos(datos) {
-    // console.log(datos);
+function verFecha() {
+  let resulFecha = sacarFecha();
+  fetch(url + resulFecha)
+    .then(function recogerRespuesta(respuesta) {
+      return respuesta.json();
+    })
+    .then(function recogerDatos(datos) {
+      // console.log(datos);
 
-    let mostrarEvento = `
+      let mostrarEvento = `
     <div>
     <p>${datos.date}</p>
     <h3>${datos.title}</h3>
@@ -29,5 +41,6 @@ fetch(url)
     <p>${datos.explanation}</p>
     </div>
     `;
-    document.getElementById("mostrar").innerHTML = mostrarEvento;
-  });
+      document.getElementById("mostrar").innerHTML = mostrarEvento;
+    });
+}
